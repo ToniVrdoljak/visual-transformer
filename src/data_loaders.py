7d445b835a86cf3258c0bfdc277973bc3917751b
Toni Vrdoljak <toni.vrdoljak4@gmail.com>
2021-04-04 19:11:12 +0200

    Implement Datasets and DataLoaders for LAD dataset (labels and attributes)

--- src/data_loaders.py
+++ src/data_loaders.py
@@ -1,10 +1,14 @@
 import os
-from torch.utils.data import DataLoader
+import torch
+from torch.utils.data import DataLoader, random_split
 from torchvision.datasets import CIFAR10, CIFAR100, ImageFolder
 from torchvision.transforms import transforms
 
+from lad_datasets import LadLabelsDataset, LadAttributesDataset
 
-__all__ = ['CIFAR10DataLoader', 'ImageNetDataLoader', 'CIFAR100DataLoader']
+
+__all__ = ['CIFAR10DataLoader', 'ImageNetDataLoader', 'CIFAR100DataLoader',
+           'LadLabelsDataLoader', 'LadAttributesDataLoader']
 
 
 class CIFAR10DataLoader(DataLoader):
@@ -81,6 +85,70 @@ class ImageNetDataLoader(DataLoader):
         self.dataset = ImageFolder(root=os.path.join(data_dir, split), transform=transform)
         super(ImageNetDataLoader, self).__init__(
             dataset=self.dataset,
+            batch_size=batch_size,
+            shuffle=True if split == 'train' else False,
+            num_workers=num_workers)
+
+
+class LadLabelsDataLoader(DataLoader):
+    def __init__(self, data_dir, split='train', image_size=224, batch_size=16, num_workers=8, train_split=0.8):
+        if split == 'train':
+            transform = transforms.Compose([
+                transforms.Resize((image_size, image_size)),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+            ])
+        else:
+            transform = transforms.Compose([
+                transforms.Resize((image_size, image_size)),
+                transforms.ToTensor(),
+                transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+            ])
+
+        lad_dataset = LadLabelsDataset(data_dir, transform=transform)
+
+        size = len(lad_dataset)
+        train_size = int(size * train_split)
+        val_size = size - train_size
+
+        train_ds, val_ds = \
+            random_split(lad_dataset, [train_size, val_size], generator=torch.Generator().manual_seed(42))
+
+        super(LadLabelsDataLoader, self).__init__(
+            dataset=train_ds if split == 'train' else val_ds,
+            batch_size=batch_size,
+            shuffle=True if split == 'train' else False,
+            num_workers=num_workers)
+
+
+class LadAttributesDataLoader(DataLoader):
+    def __init__(self, data_dir, split='train', image_size=224, batch_size=16, num_workers=8, train_split=0.8):
+        if split == 'train':
+            transform = transforms.Compose([
+                transforms.Resize((image_size, image_size)),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+            ])
+        else:
+            transform = transforms.Compose([
+                transforms.Resize((image_size, image_size)),
+                transforms.ToTensor(),
+                transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+            ])
+
+        lad_dataset = LadAttributesDataset(data_dir, transform=transform)
+
+        size = len(lad_dataset)
+        train_size = int(size * train_split)
+        val_size = size - train_size
+
+        train_ds, val_ds = \
+            random_split(lad_dataset, [train_size, val_size], generator=torch.Generator().manual_seed(42))
+
+        super(LadAttributesDataLoader, self).__init__(
+            dataset=train_ds if split == 'train' else val_ds,
             batch_size=batch_size,
             shuffle=True if split == 'train' else False,
             num_workers=num_workers)
---
+++ src/lad_datasets.py
@@ -0,0 +1,117 @@
+import pandas as pd
+from os.path import join
+import torch
+from torch.utils.data import Dataset
+from torch.utils.data.dataset import T_co
+from PIL import Image
+
+
+def get_label_list(label_list_path):
+    label_list = pd.read_csv(label_list_path, delimiter=', ', engine='python',
+                             names=['label_id', 'label', 'label_ch'])
+
+    label_list['label_code'] = label_list.index
+
+    del label_list['label_ch']
+
+    return label_list
+
+
+def get_image_list(image_list_path):
+    images = pd.read_csv(image_list_path, delimiter=', ', engine='python',
+                         names=['image_id', 'label_id', 'y1', 'x1', 'y2', 'x2', 'path'])
+
+    images['bb'] = images['y1'].map(str) + ', ' + \
+                   images['x1'].map(str) + ', ' + \
+                   images['y2'].map(str) + ', ' + \
+                   images['x2'].map(str)
+
+    images['bb'] = images['bb'].map(eval)
+
+    images.drop(columns=['x1', 'y1', 'x2', 'y2'], inplace=True)
+
+    return images
+
+
+def get_image_attributes(attributes_path):
+    img_attributes = pd.read_csv(attributes_path, delimiter=', ', engine='python',
+                                 names=['label_id', 'path', 'attributes'])
+
+    img_attributes['attributes'] = img_attributes['attributes']\
+        .map(lambda atts: [int(a) for a in atts.strip('[]').split()])
+
+    return img_attributes
+
+
+def get_image_labels(annotations_dir):
+    images = get_image_list(join(annotations_dir, 'images.txt'))
+    label_map = get_label_list(join(annotations_dir, 'label_list.txt'))
+
+    image_labels = pd.merge(images, label_map, on='label_id')
+
+    return image_labels
+
+
+def get_image_attributes_with_bb_coordinates(annotations_dir):
+    img_attributes = get_image_attributes(join(annotations_dir, 'attributes.txt'))
+    image_list = get_image_list(join(annotations_dir, 'images.txt'))
+
+    del image_list['label_id']
+
+    attributes_bb_coordinates = pd.merge(img_attributes, image_list, on='path')
+
+    return attributes_bb_coordinates
+
+
+class LadLabelsDataset(Dataset):
+    def __init__(self, ds_path, transform=None, crop_bb=True):
+        self.image_labels = get_image_labels(join(ds_path, 'LAD_annotations'))
+        self.img_dir_path = join(ds_path, 'LAD_images')
+        self.transform = transform
+        self.crop_bb = crop_bb
+
+    def __getitem__(self, index) -> T_co:
+        entry = self.image_labels.iloc[index]
+        img_path = join(self.img_dir_path, entry['path'])
+        label_code = entry['label_code']
+
+        x = Image.open(img_path).convert('RGB')
+
+        if self.crop_bb:
+            y1, x1, y2, x2 = entry['bb']
+            x = x.crop((x1, y1, x2, y2))
+
+        if self.transform:
+            x = self.transform(x)
+
+        return x, label_code
+
+    def __len__(self):
+        return self.image_labels.shape[0]
+
+
+class LadAttributesDataset(Dataset):
+    def __init__(self, ds_path, transform=None, crop_bb=True):
+        self.img_attributes = get_image_attributes_with_bb_coordinates(join(ds_path, 'LAD_annotations'))
+        self.img_dir_path = join(ds_path, 'LAD_images')
+        self.transform = transform
+        self.crop_bb = crop_bb
+
+    def __getitem__(self, item):
+        entry = self.img_attributes.iloc[item]
+        img_path = join(self.img_dir_path, entry['path'])
+        attributes = entry['attributes']
+
+        x = Image.open(img_path).convert('RGB')
+
+        if self.crop_bb:
+            y1, x1, y2, x2 = entry['bb']
+            x = x.crop((x1, y1, x2, y2))
+
+        if self.transform:
+            x = self.transform(x)
+
+        return x, torch.tensor(attributes)
+
+    def __len__(self):
+        return self.img_attributes.shape[0]
